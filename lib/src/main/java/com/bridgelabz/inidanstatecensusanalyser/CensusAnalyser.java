8c4dfe706ba0c4092134d4de7691d3091407cff0
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-26 15:14:03 +0530

    Initial Commit: Welcome to Indian state Census analyser

---
+++ lib/src/main/java/com/bridgelabz/inidanstatecensusanalyser/CensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.inidanstatecensusanalyser;
+
+public class CensusAnalyser 
+{
+	public static void main(String[] args) 
+	{
+	System.out.println("Welcome to Indian state Census Analyser");
+	}
+}
